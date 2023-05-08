b80e5564c62194ccaabc08ee2e06f198ea05926a
LofiGodam <127813456+LofiGodam@users.noreply.github.com>
2023-05-08 22:38:43 +0530

    Update shayri.py

--- ShizukaXMusic/plugins/shayri.py
+++ ShizukaXMusic/plugins/shayri.py
@@ -8,7 +8,7 @@ import asyncio
 import time
 from ShizukaXMusic import app
 import string
-from strings import get_command
+
 
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
