fc46249eed51f9a0346cc0742c98a6e16484a2c1
LofiGodam <127813456+LofiGodam@users.noreply.github.com>
2023-05-12 18:00:58 +0530

    Update start.py

--- ShizukaXMusic/plugins/bot/start.py
+++ ShizukaXMusic/plugins/bot/start.py
@@ -43,7 +43,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
            keyboard = help_pannel(_)
-           await message.reply_sticker("CAACAgUAAxkBAAEBJ1JkXSbnJgXRWHOJM1aiqCLynMysjAAC9gwAAtKK6FaOtVkYl2INzi8E)
+           await message.reply_sticker("")
            return await message.reply_photo(
                      photo=config.START_IMG_URL,
                      caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
