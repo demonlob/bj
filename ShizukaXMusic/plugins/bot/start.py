f2d02302d47c6a047247c385e88b4943b2f67cf3
Its_Eagle <127813456+LofiGodam@users.noreply.github.com>
2023-05-19 10:09:10 +0530

    Update start.py

--- ShizukaXMusic/plugins/bot/start.py
+++ ShizukaXMusic/plugins/bot/start.py
@@ -188,6 +188,19 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
+                if config.START_IMG_URL:
+
+            try:
+
+                await message.reply_sticker("CAACAgUAAxkBAAIPlmRm_HN6unSK8Ku6ap2GOdckyugRAAKxAwACk_OgV0mlBAdqK99cLwQ")
+
+                await message.reply_photo(
+
+                    photo=config.START_IMG_URL,
+
+                    caption=_["start_2"].format(
+
+                        config.MUSIC_BOT_NAME
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(config.MUSIC_BOT_NAME),
